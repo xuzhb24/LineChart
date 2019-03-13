174c98ae5b0de059de27c31d2c63d02b8d93a4d8
xuzhb <2017852261@qq.com>
2019-03-13 22:39:52 +0800

    增加模拟数据的展示

--- app/src/main/java/com/xuzhb/linechart/MainActivity.java
+++ app/src/main/java/com/xuzhb/linechart/MainActivity.java
@@ -1,13 +1,21 @@
 package com.xuzhb.linechart;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.elestic.LineChart;
+
+public class MainActivity extends Activity {
+
+    private LineChart lineChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lineChart = (LineChart) findViewById(R.id.lineChart);
+        float[] yValue = {16, 11.9f, 8.2f, 9, 12};
+        String[] dates = {"01/24", "02/02", "02/12", "02/25", "03/01"};
+        lineChart.drawData(yValue, dates);
     }
 }
